51ce59375fe8ada95263b7ef422882d823ca29c9
Brijesh <brijesh9971@gmail.com>
2016-03-06 23:02:18 +0530

    3 classes added

---
+++ Sample_Automation_Project/src/com/projectname/testscript/TestScenarios_4.java
@@ -0,0 +1,18 @@
+package com.projectname.testscript;
+
+import org.testng.annotations.Test;
+import com.projectname.modularaction.ActionClass;
+import com.projectname.utils.BaseClass;
+
+public class TestScenarios_4 {
+	
+	@Test
+	public void method(){
+		
+		BaseClass.launch_browser();
+		ActionClass.loginIntoSalesforce();
+		BaseClass.closeBrowser();
+		
+	}
+
+}
